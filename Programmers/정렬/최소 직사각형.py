1166a285ff735a3f88d51907ca3ef6f9082a185c
안유진 <yoojin.an0@gmail.com>
2024-06-08 00:29:31 +0900

    [create (최소 직사각형.py)]
    문제: https://school.programmers.co.kr/learn/courses/30/lessons/86491
    TIL: https://gencomi.tistory.com/entry/99%ED%81%B4%EB%9F%BD2%EA%B8%B0-%EC%BD%94%ED%85%8C-%EC%8A%A4%ED%84%B0%EB%94%94-2%EC%9D%BC%EC%B0%A8-TIL-Sorting

--- Programmers/정렬/최소 직사각형.py
+++ Programmers/정렬/최소 직사각형.py
@@ -1,3 +1,4 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/86491
 def solution(sizes):
     sorted_sizes = [sorted(size) for size in sizes]
     max_w = max([size[0] for size in sorted_sizes])
