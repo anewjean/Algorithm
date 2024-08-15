5422c75a19f9fc1c0b9ec6e8834e297ad255ceba
안유진 <yoojin.an0@gmail.com>
2024-08-16 00:02:01 +0900

    Co-authored-by: Gahyeon Lee <Gahyeon-Lee@users.noreply.github.com> Co-authored-by: suojae <ssuojae@users.noreply.github.com>

---
+++ 399. Evaluate Division.py
@@ -0,0 +1,34 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:  
+
+        look_up = collections.defaultdict(dict)
+
+        for (x, y), value in zip(equations, values):
+            look_up[x][y] = value
+            look_up[y][x] = 1.0/value
+
+        def DFS(x, y, visit):
+            if x not in look_up or y not in look_up:
+                return -1.0
+            
+            if y in look_up[x]: 
+                return look_up[x][y]
+            
+            for i in look_up[x]:
+                if i in visit:
+                    continue
+                visit.add(i) 
+                tmp = DFS(i, y, visit)
+
+                if tmp == -1.0: 
+                    continue 
+                else:
+                    return look_up[x][i] * tmp
+                
+            return -1.0
+
+        res=[]
+        for x, y in queries:
+            res.append(DFS(x, y, set()))
+        
+        return res
